3cd192872765b18a9019ec3d4622a7e03c7e1a45
CLacaile <clementlacaile@gmail.com>
2019-11-29 17:17:45 +0100

    register all models so they can be accessible via the admin page

--- TurboStock/app/admin.py
+++ TurboStock/app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import CEO, StoreManager, Store, AisleManager, Aisle, Product, Stock
 
-# Register your models here.
+admin.register(CEO)
+admin.register(StoreManager)
+admin.register(Store)
+admin.register(AisleManager)
+admin.register(Aisle)
+admin.register(Product)
+admin.register(Stock)
