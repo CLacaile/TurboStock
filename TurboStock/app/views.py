ccac3a6c0d881eeea8f455842c91f5d8e4aba7c3
CLacaile <clementlacaile@gmail.com>
2020-01-10 16:01:47 +0100

    added index, store, and store_detail functions

--- TurboStock/app/views.py
+++ TurboStock/app/views.py
@@ -1,3 +1,21 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Index page")
+
+def store(request):
+    """ View fonction detail page of a store """
+    stores_list = Store.objects.all()
+    context = {
+        'stores_list': stores_list,
+    }
+    return render(request, 'index.html', context=context)
+
+def store_detail(request, store_id):
+    """ View fonction detail page of a store """
+    store = Store.objects.filter(id=store_id)
+    context = {
+        'store': store,
+    }
+    return render(request, 'index.html', context=context)
